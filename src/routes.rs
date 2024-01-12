bdb9f9c29d8dc7d0a35d89e302da1abde6d942bf
Arturo Figueroa <arturo.fm.bim@gmail.com>
2024-01-12 13:36:43 +0100

    Add new endpoints

--- src/routes.rs
+++ src/routes.rs
@@ -1,11 +1,11 @@
 use crate::db::Db;
-use crate::handlers::send_token_registration_request;
+use crate::handlers::{send_token_registration_request, remove_project_and_tokens_request, refresh_token_request};
 use crate::models::{ScriptParams, TokenParams};
 use axum::{
     extract::Path,
     http::StatusCode,
     response::IntoResponse,
-    routing::{get, post},
+    routing::{get, post, put, delete},
     Json, Router,
 };
 use serde_json::json;
@@ -56,24 +56,33 @@ async fn generate_script(
 }
 
 async fn refresh_token(
-    mut db: Db,
-    Path((project_id, bk, user_id)): Path<(String, String, String)>
-) -> impl IntoResponse {
-    StatusCode::OK
+    db: Db,
+    token_params: Json<TokenParams>,
+) -> StatusCode {
+    match refresh_token_request(db, token_params.0).await {
+        Ok(_) => {
+            StatusCode::OK
+        }
+        Err(e) => {
+            error!("Error creating token task: {e:?}");
+            StatusCode::INTERNAL_SERVER_ERROR
+        }
+    }
 }
 
-async fn recreate_project(
-    mut db: Db,
-    Path((project_id, bk)): Path<(String, String)>
-) -> impl IntoResponse {
-    StatusCode::OK
-}
-
-async fn remove_project_and_tokens(
-    mut db: Db,
-    Path(project_id): Path<String>
-) -> impl IntoResponse {
-    StatusCode::OK
+async fn remove_project_and_token(
+    db: Db,
+    token_params: Json<TokenParams>,
+) -> StatusCode {
+    match remove_project_and_tokens_request(db, token_params.0).await {
+        Ok(_) => {
+            StatusCode::OK
+        }
+        Err(e) => {
+            error!("Error creating token task: {e:?}");
+            StatusCode::INTERNAL_SERVER_ERROR
+        }
+    }
 }
 
 pub fn configure_routes(pool: diesel::r2d2::Pool<diesel::r2d2::ConnectionManager<diesel::prelude::SqliteConnection>>) -> Router {
@@ -81,8 +90,7 @@ pub fn configure_routes(pool: diesel::r2d2::Pool<diesel::r2d2::ConnectionManager
         .route("/tokens", post(create_token))
         .route("/projects/:project_id/status/:bk", get(check_status))
         .route("/scripts", get(generate_script))
-        .route("/projects/:project_id/refreshToken/:bk/:user_id", post(refresh_token)) // New route
-        .route("/projects/:project_id/recreateProject/:bk", post(recreate_project))    // New route
-        .route("/projects/:project_id/remove", post(remove_project_and_tokens)) 
+        .route("/refreshToken", put(refresh_token))
+        .route("/projects", delete(remove_project_and_token)) 
         .with_state(pool)
 }
